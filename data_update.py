85025f40a09e2b3a859226fec9e8657fd17835c2
WeiJie Ou <ouweijie123@outlook.com>
2025-06-15 22:21:56 +0800

    first test

---
+++ data_update.py
@@ -0,0 +1,29 @@
+from utils import load_bais
+import sqlalchemy
+
+SQL_PASSWORDS = import.meta.env.SQL_PASSWORDS
+
+def write_to_sql(data, table_name, engine):
+    data.to_sql(
+        name=table_name,
+        con=engine,
+        if_exists="replace",
+        index=False,
+        dtype={
+            "日期": sqlalchemy.types.Date,
+            "主力合约": sqlalchemy.types.String(20),
+            "到期日": sqlalchemy.types.Date,
+            "剩余天数": sqlalchemy.types.Integer,
+        },
+    )
+
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        f"mysql+pymysql://dev:{SQL_PASSWORDS}@120.48.57.24:3306/UpdatedData?charset=utf8"
+    )
+    print("Updating IF/IC/IM data...")
+    for future_type in ["IF", "IC", "IM", "IH"]:
+        data = load_bais(future_type)
+        write_to_sql(data, f"{future_type}_data", engine)
+        print(f"{future_type} data updated successfully.")
