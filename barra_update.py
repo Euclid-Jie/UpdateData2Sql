b3e02e4f55793a89f0c7e7bdc766e42d7fe18d2a
EuclidJie@GTHTPC <ouweijie123@outlook.com>
2025-08-27 10:24:01 +0800

    fix: 修正数据检查逻辑，确保在处理数据前检查data是否为空

--- barra_update.py
+++ barra_update.py
@@ -46,7 +46,7 @@ def main():
         url = url.format(start_date, today)
         data = requests.get(url, headers=headers).json()["data"]
         all_data = pd.DataFrame()
-        if all_data.empty:
+        if data.empty:
             print(f"没有需要更新的{type}的数据")
             continue
         for key, value in data.items():
