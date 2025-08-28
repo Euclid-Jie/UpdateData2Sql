7a17e4f910a91627a5496645dd4a486727e9065e
WeiJie Ou <ouweijie123@outlook.com>
2025-08-28 22:09:13 +0800

    fix: 修正获取前一个交易日数据的逻辑，确保在周一获取周五的数据
    将基金备案数据单开yml, 周一至周五早上运行
    修改python拼写错误！！！

--- pfund_info_update.py
+++ pfund_info_update.py
@@ -10,10 +10,16 @@ if __name__ == "__main__":
     engine = sqlalchemy.create_engine(
         f"mysql+pymysql://dev:{SQL_PASSWORDS}@{SQL_HOST}:3306/UpdatedData?charset=utf8"
     )
+    # 设置为每个交易日盘前运行, 获取昨天的数据, 但是如果周一, 则获取周五的数据
+    today = np.datetime64("now").astype("datetime64[D]")
+    weekday = today.astype(object).weekday()  # Monday is 0, Sunday is 6
+    if weekday == 0:
+        delta = 3  # Monday, get Friday's data
+    else:
+        delta = 1  # Other days, get previous day's data
+    prev_trading_day = today - np.timedelta64(delta, "D")
     all_data_df = get_single_company_fund_info(
-        begin_date=(
-            np.datetime64("now").astype("datetime64[D]") - np.timedelta64(1, "D")
-        ).__str__(),
+        begin_date=str(prev_trading_day),
     )
     if len(all_data_df) > 0:
         del all_data_df["managersInfo"]
